0ddd99a1f13b2762fc586ac15b11bb33161a62bb
MortZx <mortzxd@gmail.com>
2018-05-12 14:01:01 +0100

    votechainStart.sh now working

--- votechainStart.sh
+++ votechainStart.sh
@@ -1,11 +1,18 @@
-export FABRIC_VERSION="hlfv11"
+export FABRIC_VERSION="hlfv1"
+
 echo "** STARTING FABRIC **"
 ../startFabric.sh
 ../createPeerAdminCard.sh
+
+echo "** CREATING .bna FILE **"
+composer archive create -a votechain.bna -t dir -n .
+
 echo "** INSTALLING VOTECHAIN ** "
-composer network install --archiveFile voting-network.bna --card PeerAdmin@hlfv1
+composer network install --archiveFile votechain.bna --card PeerAdmin@hlfv1
+
 echo "** STARTING VOTECHAIN **"
-composer network start --networkName vote-network --networkVersion 0.0.1 --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw
-composer card import --file networkadmin.card
+composer network start --networkName votechain --networkVersion 0.0.1 --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw
+composer card import --file admin@votechain.card
+
 echo "** DEPLOYING REST SERVER **"
-composer-rest-server -c admin@vote-network -n never -w true
+composer-rest-server -c admin@votechain -n never -w true
