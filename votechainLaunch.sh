900a7a4e4a7faa10e8fd26c5d697d2d2d0b8dff0
User <user@ECS-Mac-2018.local>
2018-05-12 15:11:23 +0100

    fixed launching network

---
+++ votechainLaunch.sh
@@ -0,0 +1,12 @@
+echo "** CREATING .bna FILE **"
+composer archive create -a votechain.bna -t dir -n .
+
+echo "** INSTALLING VOTECHAIN ** "
+composer network install --archiveFile votechain.bna --card PeerAdmin@hlfv1
+
+echo "** STARTING VOTECHAIN **"
+composer network start --networkName votechain --networkVersion 0.0.1 --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw
+# composer card import --file admin@votechain.card
+
+echo "** DEPLOYING REST SERVER **"
+composer-rest-server -c admin@votechain -n never -w true
